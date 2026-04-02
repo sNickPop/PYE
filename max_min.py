b2d6143e4b4b6f9d7ada46a831a8fa489d2a0a4b
sNickPop <nick-poplin@pm.me>
2026-04-02 15:44:01 -0400

    Add files via upload

---
+++ max_min.py
@@ -0,0 +1,21 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+            break
+    print(num)
+    try:
+        number = int(num)
+    except:
+         print('Invalid Input')
+    if smallest is None:
+        smallest = number
+    elif number < smallest:
+        smallest = number
+    if largest is None:
+        largest = number
+    elif largest < number:
+        largest = number
+print("Maximum is", largest)
+print('Minimum is', smallest)
