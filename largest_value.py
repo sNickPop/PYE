0f618aae6eb8638002801c0ffad6aa7eaa3a48bd
sNickPop <nick-poplin@pm.me>
2026-04-02 15:01:16 -0400

    Change smallest to largest value tracking

--- largest_value.py
+++ largest_value.py
@@ -1,9 +1,9 @@
-smallest_so_far = None
+largest_so_far = None
 print('Before', smallest_so_far)
 for the_num in [9, 41, 12, 3, 74, 15] :
-    if smallest_so_far is None:
-        smallest_so_far = the_num
-    elif the_num > smallest_so_far:
-        smallest_so_far = the_num
-    print(smallest_so_far, the_num)
-print('After', smallest_so_far)
+    if largest_so_far is None:
+        largest_so_far = the_num
+    elif the_num > largest_so_far:
+        largest_so_far = the_num
+    print(largest_so_far, the_num)
+print('After', largest_so_far)
