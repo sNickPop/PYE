131691b7c5241eccf12caa1872b86da928e187f6
sNickPop <nick-poplin@pm.me>
2026-04-02 15:10:07 -0400

    Add files via upload

---
+++ letter_grade.py
@@ -0,0 +1,18 @@
+score = input('Enter Score:')
+try:
+    score = float(score)
+    if score <= 1.0 and score >= 0.0:
+        if score >= 0.9:
+            print('A')
+        elif score >= 0.8:
+            print('B')
+        elif score >= 0.7:
+            print('C')
+        elif score >= 0.6:
+            print('D')
+        else:
+            print('F')
+    else:
+        print('Error: Score must be between 0.0 and 1.0')
+except:
+    quit()
